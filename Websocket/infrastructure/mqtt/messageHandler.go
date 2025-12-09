ca57cea7a15adecd51dda4db7418c492a06ddb1f
bchanona <bh274266@gmail.com>
2025-12-09 01:12:30 -0600

    Separar conexión a Rabbit

---
+++ Websocket/infrastructure/mqtt/messageHandler.go
@@ -0,0 +1,126 @@
+package mqtt
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/bchanona/websocket_backend/Websocket/domain"
+	"github.com/bchanona/websocket_backend/Websocket/infrastructure/server"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+// Procesador de mensajes MQTT
+var messageHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	apiVitals := os.Getenv("Api_Vitals")
+	apiUser := os.Getenv("Api_User")
+	payloadStr := string(msg.Payload())
+
+	var payload domain.Message
+
+	// Decodificar el JSON del mensaje recibido
+	err := json.Unmarshal([]byte(payloadStr), &payload)
+	if err != nil {
+		fmt.Println("Error al decodificar el mensaje JSON:", err)
+		return
+	}
+
+	// Enviar por WebSocket
+	go server.Manager.SendToUserDevice(payload.UserID,payload.DeviceId,payload)
+
+	var apiURL string
+	var data map[string]interface{}
+
+	
+	if payload.Spo2 != 0 {
+		apiURL = apiVitals + "/oxygen/"
+		data = map[string]interface{}{
+			"user_id":     payload.UserID,
+			"measurement": payload.Spo2,
+			"device_id":   payload.DeviceId,
+		}
+	} else if payload.Bpm != 0 {
+		apiURL = apiVitals + "/heartRate/"
+		data = map[string]interface{}{
+			"user_id":     payload.UserID,
+			"measurement": payload.Bpm,
+			"device_id":   payload.DeviceId,
+		}
+	} else if payload.Bpm2 != 0 {
+		apiURL = apiVitals + "/heartRate/"
+		data = map[string]interface{}{
+			"user_id":     payload.UserID,
+			"measurement": payload.Bpm2,
+			"device_id":   payload.DeviceId,
+		}
+	} else if payload.Temperature != 0 {
+		apiURL = apiVitals + "/temperature/"
+		data = map[string]interface{}{
+			"user_id":     payload.UserID,
+			"measurement": payload.Temperature,
+			"device_id":   payload.DeviceId,
+		}
+	}
+
+	// Validación de condiciones anormales
+	isAnormal := false
+	var mensaje string
+
+	if payload.Temperature > 37.5 {
+		isAnormal = true
+		mensaje += fmt.Sprintf("Temperatura alta: %.1f°C. ", payload.Temperature)
+	}
+	if payload.Bpm < 60 || payload.Bpm > 100 {
+		isAnormal = true
+		mensaje += fmt.Sprintf("Ritmo cardíaco anormal: %d bpm. ", payload.Bpm)
+	}
+	if payload.Spo2 < 95 {
+		isAnormal = true
+		mensaje += fmt.Sprintf("Oxigenación baja: %d%%. ", payload.Spo2)
+	}
+
+	if isAnormal {
+		notifData := map[string]interface{}{
+			"user_id": payload.UserID,
+			"body":    mensaje,
+			"reading": false, 
+		}
+
+		notifJSON, err := json.Marshal(notifData)
+		if err != nil {
+			fmt.Println("Error al convertir notificación a JSON:", err)
+		} else {
+			resp, err := http.Post(apiUser + "/user/saveNotification", "application/json", bytes.NewBuffer(notifJSON))
+			if err != nil {
+				fmt.Println("Error al enviar notificación:", err)
+			} else {
+				defer resp.Body.Close()
+				fmt.Println("Notificación enviada con status:", resp.Status)
+			}
+		}
+	}
+
+	// Enviar datos a la API de vitales si hay URL definida
+	if apiURL != "" {
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			fmt.Println("Error al convertir data a JSON:", err)
+			return
+		}
+
+		resp, err := http.Post(apiURL, "application/json", bytes.NewBuffer(jsonData))
+		if err != nil {
+			fmt.Println("Error al enviar datos a la API:", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		fmt.Printf("API response: %s\n", resp.Status)
+	} else {
+		fmt.Println("No valid data was found to send to the API")
+	}
+
+	fmt.Printf("Message received in [%s]: %s\n", msg.Topic(), payloadStr)
+}
